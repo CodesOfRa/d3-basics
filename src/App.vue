b20f43cc33070be7e22a9c4517e03ee18ee4feb5
CodesOfRa <ramona.biscoveanu@gmail.com>
2019-05-05 19:15:32 +0200

    :books: clean up

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,37 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <line-chart :columns="columns" :data="data" :co2="co2"></line-chart>
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <line-chart :defaultData="defaultData" :data="data"></line-chart>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 import LineChart from "./components/LineChart.vue";
-// import data from "../data.csv";
 import * as d3 from "d3";
-// const d3 = { ...require("d3") };
+
 export default {
   name: "app",
   components: {
-    HelloWorld,
     LineChart
   },
   data() {
     return {
       data: [],
-      columns: []
+      defaultData: {
+        width: 1200,
+        heigth: 500,
+        margin: {
+          top: 100,
+          bottom: 100
+        },
+        year: {
+          min: 1960,
+          max: 2014
+        },
+        value: {
+          max: 38000000
+        }
+      }
     };
   },
   mounted: function() {
@@ -30,23 +40,12 @@ export default {
   methods: {
     async getData() {
       let data = await d3.csv("../co2.csv");
-      // console.log(data[0]);
-      data.columns.map(d => {
-        if (!isNaN(d)) {
-          this.columns.push(d);
-          //convert strings to numbers
-          data.map(
-            data => (data[d] = +data[d])
-            // console.log("_______" + data["Country Name"] + "-----" + data[d])
-          );
-        }
-      });
       var co2 = [];
       data.map(item => {
         let country = { country: item["Country Name"], values: [] };
         Object.keys(item).map(key => {
           if (!isNaN(key)) {
-            // console.log(key);
+            item[key] = +item[key];
             let aux = { year: key, value: item[key] };
             country.values.push(aux);
           }
@@ -54,7 +53,7 @@ export default {
         co2.push(country);
         // console.log(Object.keys(item));
       });
-      console.log(co2);
+
       this.data = co2;
     }
   }
--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
     <!-- <span>Line Chart</span> -->
-    <svg width="1200px" height="500px">
+    <svg :width="width" :height="heigth">
       <g ref="xAxis" transform="translate(0, 400)"></g>
       <g ref="yAxis" transform="translate(100, 0)"></g>
       <g v-if="data.length === 263">
         <path
-          v-for="(datas,index) in data"
-          :id="datas.country"
-          :key="datas.country"
-          :d="generateLine(datas.values,index)"
+          v-for="(d,index) in data"
+          :id="d.country"
+          :key="d.country"
+          :d="generateLine(d.values,index)"
           :stroke="colours(index)"
           stroke-width="1"
           fill="none"
-          @mouseover="onClick(datas)"
-          @click="onClick(datas)"
+          @mouseover="onClick(d)"
+          @click="onClick(d)"
         ></path>
         <!-- <circle
           v-for="(a,i) in datas.values"
@@ -28,7 +28,7 @@
         ></circle>-->
       </g>
 
-      <!-- <g v-for="(d,i) in data" :key="d">
+      <g v-for="(d,i) in data" :key="d">
         <circle
           v-for="(a,index) in d.values"
           :key="index"
@@ -37,11 +37,10 @@
           :stroke="colours(i)"
           :cx="generateCircleCX(a)"
           :cy="generateCircleCY(a)"
-          opacity="0.4c"
+          opacity="0.4"
         ></circle>
-      </g>-->
+      </g>
     </svg>
-    <svg width="1200px"></svg>
   </div>
 </template>
 <script>
@@ -54,83 +53,63 @@ const d3 = {
   ...require("d3-selection")
 };
 export default {
-  props: ["columns", "data", "co2"],
+  props: ["data", "defaultData"],
   data: function() {
-    return { d: [], lineX: d3.line() };
+    return { path: d3.line() };
   },
   computed: {
+    width: function() {
+      return this.defaultData.width + "px";
+    },
+    heigth: function() {
+      return this.defaultData.heigth + "px";
+    },
     line: function() {
       this.generateLine();
     },
     colours: function() {
-      //   console.log(index);
       return d3.scaleSequential(d3.interpolateViridis).domain([0, 265]);
+    },
+    xScale: function() {
+      var xScale = d3
+        .scaleLinear()
+        .domain([this.defaultData.year.min, this.defaultData.year.max])
+        .range([this.defaultData.margin.bottom, this.defaultData.width]);
+      return xScale;
+    },
+    yScale: function() {
+      var yScale = d3
+        .scaleLinear()
+        .domain([0, this.defaultData.value.max])
+        .range([
+          this.defaultData.heigth - this.defaultData.margin.top,
+          this.defaultData.margin.bottom
+        ]);
+      return yScale;
     }
   },
   mounted() {
     this.$nextTick(
       function() {
-        //   this.width = this.$refs.lineChart.offsetWidth;
-        //   this.height = this.$refs[this.id].offsetHeight;
-        //   this.drawChart();
-        var xScale = d3
-          .scaleLinear()
-          .domain([1960, 2014]) //input
-          .range([100, 1200]);
-        var yScale = d3
-          .scaleLinear()
-          .domain([0, 38000000])
-          .range([400, 100]);
-        console.log(this.$refs);
-        d3.select(this.$refs.xAxis).call(d3.axisBottom(xScale).ticks(7));
-        d3.select(this.$refs.yAxis).call(d3.axisLeft(yScale).ticks(7));
+        d3.select(this.$refs.xAxis).call(d3.axisBottom(this.xScale).ticks(7));
+        d3.select(this.$refs.yAxis).call(d3.axisLeft(this.yScale).ticks(7));
       }.bind(this)
     );
   },
   methods: {
     generateCircleCX: function(data) {
-      //   console.log(data);
-      var xScale = d3
-        .scaleLinear()
-        .domain([1960, 2014]) //input
-        .range([100, 1200]);
-      return xScale(data.year);
+      return this.xScale(data.year);
     },
     generateCircleCY: function(data) {
-      var yScale = d3
-        .scaleLinear()
-        .domain([0, 38000000])
-        .range([400, 100]);
-      return yScale(data.value);
+      return this.yScale(data.value);
     },
-    generateLine: function(data, index) {
-      // console.log("_____", data, index);
-      var xScale = d3
-        .scaleLinear()
-        .domain([1960, 2014]) //input
-        .range([100, 1200]);
-      var yScale = d3
-        .scaleLinear()
-        .domain([0, 38000000])
-        .range([400, 100]);
-      this.lineX
-        .x(function(d, i) {
-          return xScale(d.year);
-        }) // set the x values for the line generator
-        .y(function(d, i) {
-          return yScale(d.value);
-        });
+    generateLine: function(data) {
+      this.path.x(d => this.xScale(d.year)).y(d => this.yScale(d.value));
 
-      return this.lineX(data);
+      return this.path(data);
     },
     onClick: function(e) {
       console.log(e);
-    },
-    generateColour: function(index) {
-      console.log(index);
-      //   if (index !== this.data.length) {
-      return colours[index];
-      //   }
     }
   }
 };
